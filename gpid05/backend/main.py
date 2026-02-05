2defd02b76e07ef260f3b0f4dcd52faf7300be09
Bmccomiskey <braylonmccomiskey@icloud.com>
2026-02-05 17:20:31 +0000

    added main.py implementation for the app to run the backend

---
+++ gpid05/backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import auth_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health():
+    return {"ok": True}
+
+app.include_router(auth_router, prefix="/auth")
