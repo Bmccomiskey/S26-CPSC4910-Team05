5e3665cff1a7c40839966e26b2aff6107b7155c0
Apope5 <apope4@clemson.edu>
2026-02-10 16:23:54 -0500

    Rewrite db.py to connect to AWS DB
    Requires mysql-connector, "pip install mysql-connector-python boto3." Also included some example code for entering, updating, and fetching data.

--- gpid05/backend/db.py
+++ gpid05/backend/db.py
@@ -1,23 +1,56 @@
-# This file is a temporary database until the DB server is setup and linked
+#This file is for establishing connectivity with the MySQL Database
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import mysql.connector
+import boto3
 
-DATABASE_URL = "sqlite:///./gpid.db" # this will make a gpid.db in the backend folder
+password = "CPSC4910"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+conn = None
+try:
+    conn = mysql.connector.connect(
+        host='cpsc4910-s26.cobd8enwsupz.us-east-1.rds.amazonaws.com',
+        port=3306,
+        database='Team05_DB',
+        user='Team05',
+        password=password,
+        ssl_disabled=False,
+    ssl_ca='/certs/global-bundle.pem'
+    )
+    cur = conn.cursor()
+    cur.execute('SELECT VERSION();')
+    print(cur.fetchone()[0])
+    cur.close()
+except Exception as e:
+    print(f"Database error: {e}")
+    raise
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#confirm connection established
+if conn:
+    print("Connection established successfully")
 
-class Base(DeclarativeBase):
-    pass
+#connection testing
+if conn: 
+    cursor = conn.cursor()
+    """
+    query = "INSERT INTO Version_Info VALUES (%s,%s,%s,%s,%s)"
+    values = ("5","2","2026-02-10", "Incentive Program", "Incentive Application")
+    cursor.execute(query, values)
+    conn.commit()
+    """
+    query = "SELECT * FROM Version_Info"
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    cursor.execute(query)
+
+    # Fetch results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    query = "UPDATE Version_Info SET ProductDesc = 'Test' WHERE TeamNum = 5"
+    cursor.execute(query)
+    conn.commit()
+
+#close connection
+if conn: 
+    conn.close()
+    print("Connection closed")
