0d1259997ec672645e25fd43a7ee6a5a730a6acc
Apope5 <apope4@clemson.edu>
2026-02-12 14:53:32 -0500

    Created functions for connecting and disconnecting to/from database

--- gpid05/backend/db.py
+++ gpid05/backend/db.py
@@ -3,42 +3,38 @@
 import mysql.connector
 import boto3
 
-password = password
-
-conn = None
-try:
-    conn = mysql.connector.connect(
+def dbConnect(password):   
+    conn = None
+    try:
+        conn = mysql.connector.connect(
         host='cpsc4910-s26.cobd8enwsupz.us-east-1.rds.amazonaws.com',
         port=3306,
         database='Team05_DB',
         user='Team05',
         password=password,
         ssl_disabled=False,
-    ssl_ca='/certs/global-bundle.pem'
+        ssl_ca='/certs/global-bundle.pem'
     )
-    cur = conn.cursor()
-    cur.execute('SELECT VERSION();')
-    print(cur.fetchone()[0])
-    cur.close()
-except Exception as e:
-    print(f"Database error: {e}")
-    raise
+        cur = conn.cursor()
+        cur.execute('SELECT VERSION();')
+        print(cur.fetchone()[0])
+        cur.close()
+    except Exception as e:
+        print(f"Database error: {e}")
 
 #confirm connection established
-if conn:
-    print("Connection established successfully")
+    if conn:
+     print("Connection established successfully")
+
+    return conn
 
 #connection testing
-if conn: 
-    cursor = conn.cursor()
-    """
-    query = "INSERT INTO Version_Info VALUES (%s,%s,%s,%s,%s)"
-    values = ("5","2","2026-02-10", "Incentive Program", "Incentive Application")
-    cursor.execute(query, values)
-    conn.commit()
-    """
-    query = "SELECT * FROM Version_Info"
+conn = dbConnect()
 
+if conn:
+    cursor = conn.cursor()
+    
+    query = "SELECT * FROM Version_Info"
     cursor.execute(query)
 
     # Fetch results
@@ -51,6 +47,8 @@ if conn:
     conn.commit()
 
 #close connection
-if conn: 
+def dbConnClose(conn):
     conn.close()
     print("Connection closed")
+
+dbConnClose(conn)
