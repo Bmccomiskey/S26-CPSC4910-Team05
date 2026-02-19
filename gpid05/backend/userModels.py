dc257ec64172167d0a86d89692c2b2a3bf050999
AKAP4 <ajkapoor2004@gmail.com>
2026-02-19 09:57:56 -0500

    adds is_active field to user model for deactivating accounts

--- gpid05/backend/userModels.py
+++ gpid05/backend/userModels.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from db import Base
 
@@ -10,5 +10,6 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
     role: Mapped[str] = mapped_column(String(20), nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
 
     reset_tokens = relationship("PasswordResetToken", back_populates="user")
